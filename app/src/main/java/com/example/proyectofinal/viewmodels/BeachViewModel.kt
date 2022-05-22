ea244703258925bd8f61183131ba95dad48385b0
AlePantu <alepantuliano@hotmail.com>
2022-05-22 12:55:29 -0300

    Circular seekBars Aforo Temp Parking on DataBeachView

--- app/src/main/java/com/example/proyectofinal/viewmodels/BeachViewModel.kt
+++ app/src/main/java/com/example/proyectofinal/viewmodels/BeachViewModel.kt
@@ -17,7 +17,15 @@ class BeachViewModel : ViewModel() {
     private lateinit var pcAforo : CircularSeekBar
     private lateinit var aforoView: TextView
 
+    private lateinit var pcTemp : CircularSeekBar
+    private lateinit var tempView: TextView
+
+    private lateinit var pcPark : CircularSeekBar
+    private lateinit var parkView: TextView
+
     private var aforo : Float = 0F
+    private var temp : Float = 0F
+    private var park : Float = 0F
 
 
     fun showDataBeach(idPlaya: String, v: View) {
@@ -28,16 +36,26 @@ class BeachViewModel : ViewModel() {
 
         pcAforo = v.findViewById(R.id.pcaforo)
 
+        tempView = v.findViewById(R.id.tempTextView)
+        pcTemp = v.findViewById(R.id.pctemp)
+
+        parkView = v.findViewById(R.id.parkTextView)
+        pcPark = v.findViewById(R.id.pcpark)
+
         db.collection("dtis").document(idPlaya).get().addOnSuccessListener {
-            pcAforo.max = 500F
+            pcAforo.max = (it.get("maxAforo").toString()).toFloat()
+            pcPark.max = (it.get("maxPark").toString()).toFloat()
             aforo = (it.get("aforo").toString()).toFloat()
-
-
+            temp =  (it.get("temperatura").toString()).toFloat()
+            park = (it.get("parking").toString()).toFloat()
 
             nameView.text = it.get("nombre").toString()
-            //beachTemp.text = it.get("temperatura").toString()
-           aforoView.text = aforo.toString()
+           aforoView.text = it.get("aforo").toString()
+            tempView.text = it.get("temperatura").toString()
+            parkView.text = it.get("parking").toString()
             pcAforo.progress = aforo
+            pcTemp.progress = temp
+            pcPark.progress = park
         }
 
 
